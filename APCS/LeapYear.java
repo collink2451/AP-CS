400481826534847b6ef8280fcff96447de9e3af7
collink2451 <collinkoldoff@gmail.com>
2020-11-10 07:17:20 -0600

    Update LeapYear.java

--- APCS/LeapYear.java
+++ APCS/LeapYear.java
@@ -24,10 +24,18 @@ public class LeapYear {
     }
 
     public static void isLeapYear(int year) {
-        if (year % 400 == 0) {
-            System.out.println(year + " is a leap year.\n");
-            return;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println(year + " is a leap year.\n");
+                } else {
+                    System.out.println(year + " is NOT a leap year.\n");
+                }
+            } else {
+                System.out.println(year + " is a leap year.\n");
+            }
+        } else {
+            System.out.println(year + " is NOT a leap year.\n");
         }
-        System.out.println(year + " is NOT a leap year.\n");
     }
 }
