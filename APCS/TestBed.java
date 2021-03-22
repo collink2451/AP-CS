4555fda515836ae935f9fc30164ae5264cd4a403
collinkoldoff-KF <61240743+collink2451@users.noreply.github.com>
2021-03-21 23:38:24 -0500

    Update TestBed.java

--- APCS/TestBed.java
+++ APCS/TestBed.java
@@ -1,7 +1,14 @@
 package APCS;
 
+import java.util.ArrayList;
+
 public class TestBed {
     public static void main(String[] args) {
-        
+        ArrayList<Double> numbers = new ArrayList<Double>();
+        numbers.add(1.0);
+
+        numbers.remove(0);
+
+        System.out.print(numbers.size());
     }
 }
