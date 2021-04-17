3804e34b88eeb4ea9e8f62bfa80ce06f776dea74
collink2451 <collinkoldoff@gmail.com>
2021-04-17 12:52:16 -0500

    Update TestBed.java

--- APCS/TestBed.java
+++ APCS/TestBed.java
@@ -1,14 +1,15 @@
 package APCS;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class TestBed {
     public static void main(String[] args) {
-        ArrayList<Double> numbers = new ArrayList<Double>();
-        numbers.add(1.0);
-
-        numbers.remove(0);
-
-        System.out.print(numbers.size());
+        File[] files = new File("C:\\Users\\Collin\\Documents\\GitHub\\AP-CS\\APCS").listFiles();
+        Arrays.sort(files, Comparator.comparingLong(File::lastModified));
+        for (File item : files) {
+            System.out.println(item.toString());
+        }
     }
 }
